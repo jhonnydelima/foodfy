f79c11bd55fe8584c0c69895b85cadb2e2590277
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2020-12-30 15:55:45 -0300

    refactoring foodfy

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,19 +1,19 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const recipes = document.querySelectorAll('.recipe')
 
-for (let recipe of recipes) {
-    recipe.addEventListener("click", function(){
+for ( let recipe of recipes ) {
+    recipe.addEventListener("click", function() {
         const recipeId = recipe.getAttribute('id')
         const recipeName = recipe.querySelector('.recipe__name').textContent
         const chefName = recipe.querySelector('.recipe__chef-name').textContent
 
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('img').src = `/public/assets/${recipeId}.png`
-        modalOverlay.querySelector('.recipe-name').innerHTML = recipeName
-        modalOverlay.querySelector('.chef-name').innerHTML = chefName
+        modalOverlay.querySelector('.recipe-name').textContent = recipeName
+        modalOverlay.querySelector('.chef-name').textContent = chefName
     })
 }
 
-document.querySelector('.close-modal').addEventListener("click", function(){
+document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove('active')
 })
